123dc31d021d09842f478421e8d12c39602b4e16
ecsd-matthew-song <44193019+ecsd-matthew-song@users.noreply.github.com>
2021-02-05 15:26:26 +0800

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -1,44 +1,27 @@
-variable "ami" {
+variable ami {}
+variable subnet_id {}
+variable vpc_security_group_ids {
+  type = list
 }
-
-variable "num_webs" {
-}
-
-variable "subnet_id" {
-}
-
-variable "vpc_security_group_id" {
-}
-
-variable "identity" {
-}
-
-resource "aws_key_pair" "training" {
-  key_name   = "${var.identity}-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+variable identity {}
+variable key_name {}
+variable private_key {}
+variable num_webs {
+  default = "2"
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = "t2.nano"
-  count         = var.num_webs
-
+  ami                    = var.ami
+  instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.vpc_security_group_id]
-
-  key_name = aws_key_pair.training.id
-
-  tags = {
-    Name       = "web ${count.index + 1}/${var.num_webs}"
-    Identity   = var.identity
-    Created-by = "Terraform"
-  }
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name               = var.key_name
+  count                  = 2
 
   connection {
-    type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/.ssh/id_rsa")
-    host        = aws_instance.web.*.public_ip
+    private_key = var.private_key
+    host        = self.public_ip
   }
 
   provisioner "file" {
@@ -51,13 +34,19 @@ resource "aws_instance" "web" {
       "sudo sh /tmp/assets/setup-web.sh",
     ]
   }
+
+  tags = {
+    "Identity"    = var.identity
+    "Name"        = "Student ${count.index + 1}/${var.num_webs}"
+    "Environment" = "Training"
+  }
+
 }
 
 output "public_ip" {
-  value = [aws_instance.web.*.public_ip]
+  value = aws_instance.web[*].public_ip
 }
 
 output "public_dns" {
-  value = [aws_instance.web.*.public_dns]
+  value = aws_instance.web[*].public_dns
 }
-
