fa96e2b7ef0c97376af2950f1980e46483476e66
ecsd-matthew-song <44193019+ecsd-matthew-song@users.noreply.github.com>
2021-02-05 15:27:29 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,8 +34,16 @@ module "server" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   identity               = var.identity
+  key_name               = module.keypair.key_name
   private_key            = module.keypair.private_key_pem
 
+}
+
+module "keypair" {
+  source  = "mitchellh/dynamic-keys/aws"
+  version = "2.0.0"
+  path    = "${path.root}/keys"
+  name    = "${var.identity}-key"
 }
 
 output "public_ip" {
